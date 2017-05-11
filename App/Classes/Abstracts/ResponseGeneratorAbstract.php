0a7ea27d99d4d92c486fa9ee53a9462570a2f5fa
pentagonal <pentagonaldevelopment@gmail.com>
2017-05-11 14:23:40 +0700

    Sungguh Keterlaluan

--- App/Classes/Abstracts/ResponseGeneratorAbstract.php
+++ App/Classes/Abstracts/ResponseGeneratorAbstract.php
@@ -247,10 +247,10 @@ abstract class ResponseGeneratorAbstract
 
         if ($this->recheckMimeType || strpos($this->mimeType, '/') === false) {
             if (preg_match(
-                    '/(?:(?:[^/]*)(?:\\\+|\/+))?(html?|javascript|calendar|css|plain)/',
-                    $this->mimeType,
-                    $match
-                ) && !empty($match[1])
+                '/(?:(?:[^/]*)(?:\\\+|\/+))?(html?|javascript|calendar|css|plain)/',
+                $this->mimeType,
+                $match
+            ) && !empty($match[1])
             ) {
                 $this->mimeType = $this->mimeType == 'htm'
                     ? 'html'
@@ -262,10 +262,10 @@ abstract class ResponseGeneratorAbstract
             } elseif (strpos($this->mimeType, 'sgm') !== false) {
                 $this->mimeType = 'text/sgml';
             } elseif (preg_match(
-                    '/(?:(?:[^/]*)(?:\\\+|\/+))?(ja?son|xml|ogg|pdf|postscript|zip|ttf2?)/',
-                    $this->mimeType,
-                    $match
-                ) && !empty($match[1])
+                '/(?:(?:[^/]*)(?:\\\+|\/+))?(ja?son|xml|ogg|pdf|postscript|zip|ttf2?)/',
+                $this->mimeType,
+                $match
+            ) && !empty($match[1])
             ) {
                 if ($match[1] == 'jason') {
                     $match[1] = 'json';
@@ -274,10 +274,10 @@ abstract class ResponseGeneratorAbstract
                 }
                 $this->mimeType = 'application/' . $match[1];
             } elseif (preg_match(
-                    '/(?:(?:[^/]*)(?:\\\+|\/+))?(jpe?g?|png|w?bmp|gif|pbm|tif(?:f+)?|png|ppm|ras|xbm|xpm|xwd)/',
-                    $this->mimeType,
-                    $match
-                )
+                '/(?:(?:[^/]*)(?:\\\+|\/+))?(jpe?g?|png|w?bmp|gif|pbm|tif(?:f+)?|png|ppm|ras|xbm|xpm|xwd)/',
+                $this->mimeType,
+                $match
+            )
                 && !empty($match[1])
             ) {
                 if ($match[1] == 'wbmp') {
