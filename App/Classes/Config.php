38ec33b0f34b4d8b799b77cdd45bb9ec8e3888f0
pentagonal <pentagonaldevelopment@gmail.com>
2017-05-14 21:10:39 +0700

    Nambah Saitik ae bengi iki ... yaa yaaa..

--- App/Classes/Config.php
+++ App/Classes/Config.php
@@ -8,7 +8,7 @@ use Apatis\Exceptions\InvalidArgumentException;
  * Class Config
  * @package PentagonalProject\ProjectSeventh
  */
-class Config
+class Config implements \ArrayAccess
 {
     /**
      * @var CollectionFetch
@@ -24,6 +24,7 @@ class Config
      * @var CollectionFetch
      */
     protected $lastCollection;
+
     /**
      * Config constructor.
      * @param array $setting
@@ -48,6 +49,18 @@ class Config
         }
 
         return $this->collection->fetch($key, $default);
+    }
+
+    /**
+     * Offset Exists
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function exist($key) : bool
+    {
+        return $this->get($key, false) !== false
+            && $this->get($key, true) !== true;
     }
 
     /**
@@ -99,6 +112,7 @@ class Config
                 'Invalid key name given! Key config must be as a string!'
             );
         }
+
         if (($count = preg_match_all('/(?:^[^\[]+)|\[[^]]*\]/', $key, $matches)) > 1) {
             // Does the index contain array notation
             $the_key = null;
@@ -133,5 +147,112 @@ class Config
         }
 
         $this->collection->set($key, $values);
+    }
+
+    /**
+     * Remove Key from nested selector
+     *
+     * @param string $key
+     */
+    public function remove($key)
+    {
+        if (!is_string($key) && !is_numeric($key)) {
+            throw new InvalidArgumentException(
+                'Invalid key name given! Key config to remove must be as a string!'
+            );
+        }
+
+        if (!$this->exist($key)) {
+            return;
+        }
+
+        if (($count = preg_match_all('/(?:^[^\[]+)|\[[^]]*\]/', $key, $matches)) > 1) {
+            $firstKey = reset($matches[0]);
+            $keyName  = $firstKey;
+            $tmp      = $this->collection[$keyName];
+            if (!is_array($tmp)) {
+                return;
+            }
+            array_shift($matches[0]);
+            $unsetPosition = 0;
+            foreach ($matches[0] as $keyNum => $keyName) {
+                $keyName = trim($keyName, '[]');
+                if ($unsetPosition <> $keyNum &&
+                    (!is_array($tmp) || ! array_key_exists($keyName, $tmp))
+                ) {
+                    return;
+                }
+
+                $unsetPosition++;
+                $tmp = $tmp[$keyName];
+            }
+
+            $tmp = $this->collection[$firstKey];
+            $currentUnsetPosition = 0;
+            // binding anonymous function to handle array reference
+            $recursiveUnset = function (
+                &$array,
+                $unwanted_key
+            ) use (
+                $unsetPosition,
+                &$currentUnsetPosition,
+                &$recursiveUnset
+) {
+                $currentUnsetPosition++;
+                if ($unsetPosition !== $currentUnsetPosition) {
+                    if (array_key_exists($unwanted_key, $array)) {
+                        unset($array[$unwanted_key]);
+                    }
+                    // stop
+                    return;
+                }
+                foreach ($array as &$value) {
+                    if (is_array($value)) {
+                        $recursiveUnset($value, $unwanted_key);
+                    }
+                }
+            };
+
+            // call closure to binding reference
+            $recursiveUnset($tmp, $keyName);
+            $this->collection[$firstKey] = $tmp;
+            unset($tmp);
+            return; // stop
+        }
+
+        unset($this->collection[$key]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset) : bool
+    {
+        return $this->exist($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        return $this->set($offset, $value);
+    }
+
+    /**
+     * @param int|string $offset
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        $this->remove($offset);
     }
 }
