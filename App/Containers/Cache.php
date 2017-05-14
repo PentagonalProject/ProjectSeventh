d89ed40f498ec5c015b06b0e7e1c5139f17c4514
pentagonal <pentagonaldevelopment@gmail.com>
2017-05-14 21:14:17 +0700

    Iki kenek gawe nyimpen rondo asli ta aku gak goroh

---
+++ App/Containers/Cache.php
@@ -0,0 +1,42 @@
+<?php
+/* ------------------------------------------------------ *\
+ |                   CONTAINER CACHE                      |
+\* ------------------------------------------------------ */
+
+/**
+ * File for Slim Container for 'database'
+ * Handle Database abstraction stored object (PDO)
+ */
+namespace {
+
+    use PentagonalProject\ProjectSeventh\Config;
+    use phpFastCache\Cache\ExtendedCacheItemPoolInterface;
+    use phpFastCache\CacheManager;
+    use phpFastCache\Exceptions\phpFastCacheDriverCheckException;
+    use Slim\Container;
+
+    return function (Container $container) : ExtendedCacheItemPoolInterface {
+        /**
+         * @var Config $config
+         */
+        $config = $container[CONTAINER_CONFIG];
+        // add fix to prevent trigger error
+        if ($config['cache[driver]'] == 'Auto') {
+            $availableDriver = CacheManager::getStaticSystemDrivers();
+            foreach ($availableDriver as $driver) {
+                try {
+                    CacheManager::getInstance($driver, $config['cache[config]']);
+                    $config['cache[driver]'] = $driver;
+                    break;
+                } catch (phpFastCacheDriverCheckException $e) {
+                    continue;
+                }
+            }
+        }
+
+        return CacheManager::getInstance(
+            $config['cache[driver]'],
+            $config['cache[config]']
+        );
+    };
+}
