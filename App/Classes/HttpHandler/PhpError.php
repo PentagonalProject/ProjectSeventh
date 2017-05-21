2868418227c3e6c1441dbc488b40b3ffd8d54534
pentagonal <pentagonaldevelopment@gmail.com>
2017-05-21 15:29:45 +0700

    Rasane jan uhhhh ahhhh usluk usluk ahhhh

--- App/Classes/HttpHandler/PhpError.php
+++ App/Classes/HttpHandler/PhpError.php
@@ -1,5 +1,6 @@
 <?php
 namespace PentagonalProject\ProjectSeventh\HttpHandler;
+
 use Monolog\Logger;
 use PentagonalProject\ProjectSeventh\Application;
 use Psr\Http\Message\ResponseInterface;
@@ -22,7 +23,7 @@ class PhpError extends \Slim\Handlers\PhpError
      * @param bool $displayErrorDetails Set to true to display full details
      * @param Application $app
      */
-    public function __construct($displayErrorDetails = false, Application $app)
+    public function __construct($displayErrorDetails = false, Application $app = null)
     {
         $this->app = $app;
         parent::__construct($displayErrorDetails);
@@ -36,16 +37,18 @@ class PhpError extends \Slim\Handlers\PhpError
         ResponseInterface $response,
         \Throwable $error
     ) {
-        /** @var Logger $log */
-        $log = $this->app[CONTAINER_LOG];
-        $log->error(
-            $error->getMessage(),
-            [
-                'file' => $error->getFile(),
-                'code' => $error->getCode(),
-                'line' => $error->getLine()
-            ]
-        );
+        if ($this->app instanceof Application) {
+            /** @var Logger $log */
+            $log = $this->app[CONTAINER_LOG];
+            $log->error(
+                $error->getMessage(),
+                [
+                    'file' => $error->getFile(),
+                    'code' => $error->getCode(),
+                    'line' => $error->getLine()
+                ]
+            );
+        }
 
         return parent::__invoke($request, $response, $error);
     }
