a451c6bdab4ac2c89445bc7bbf41d9d3f880ad3f
pentagonal <pentagonaldevelopment@gmail.com>
2017-05-23 17:28:03 +0700

    Mbenakno cug

--- App/Containers/Environment.php
+++ App/Containers/Environment.php
@@ -19,17 +19,17 @@ namespace {
      */
     return function () : Environment {
         $server = $_SERVER;
-        if (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off'
+        if (!empty($server['HTTPS']) && strtolower($server['HTTPS']) !== 'off'
             // hide behind proxy / maybe cloud flare cdn
-            || isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
-            && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
-            || !empty($_SERVER['HTTP_FRONT_END_HTTPS'])
-            && strtolower($_SERVER['HTTP_FRONT_END_HTTPS']) !== 'off'
+            || isset($server['HTTP_X_FORWARDED_PROTO'])
+            && $server['HTTP_X_FORWARDED_PROTO'] === 'https'
+            || !empty($server['HTTP_FRONT_END_HTTPS'])
+            && strtolower($server['HTTP_FRONT_END_HTTPS']) !== 'off'
         ) {
             // detect if non standard protocol
-            if ($_SERVER['SERVER_PORT'] == 80
-                && (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
-                    || isset($_SERVER['HTTP_FRONT_END_HTTPS'])
+            if ($server['SERVER_PORT'] == 80
+                && (isset($server['HTTP_X_FORWARDED_PROTO'])
+                    || isset($server['HTTP_FRONT_END_HTTPS'])
                 )
             ) {
                 $server['SERVER_PORT'] = 443;
